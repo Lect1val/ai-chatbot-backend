6d55ec03be479f1e15cdd848d0d7d24de4d47a54
Thanakorn Sutad <lectival857@gmail.com@>
2024-07-11 17:02:53 +0700

    feat: handle intent Aster arcade URL

--- python_chatbot/chatbot_project/dialogflow_integration/views.py
+++ python_chatbot/chatbot_project/dialogflow_integration/views.py
@@ -8,26 +8,36 @@ from google.cloud import dialogflow
 @csrf_exempt
 def dialogflow_session(request):
     if request.method == 'POST':
-        # Load JSON data from request body
         data = json.loads(request.body)
+        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
         session_id = data.get('session_id', 'default_session')
         text = data.get('text', 'Hello')
 
-        # Set up Dialogflow session
-        project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-        session = f"projects/{project_id}/agent/sessions/{session_id}"
+        session_path = f"projects/{project_id}/agent/sessions/{session_id}"
         credentials_json = json.loads(os.getenv('GOOGLE_CREDENTIALS_JSON'))
         credentials = service_account.Credentials.from_service_account_info(credentials_json)
 
-        # Create a Dialogflow client
         session_client = dialogflow.SessionsClient(credentials=credentials)
         text_input = dialogflow.TextInput(text=text, language_code="en-US")
         query_input = dialogflow.QueryInput(text=text_input)
-        response = session_client.detect_intent(request={'session': session, 'query_input': query_input})
+        response = session_client.detect_intent(request={'session': session_path, 'query_input': query_input})
 
-        # Send response
-        return JsonResponse({
-            "fulfillmentText": response.query_result.fulfillment_text
-        })
+        intent_name = response.query_result.intent.display_name
 
-    return JsonResponse({"error": "This endpoint supports only POST method."})
+        if intent_name == 'session_search':
+            parameters = response.query_result.parameters
+            session_name = parameters.get('session_name') if 'session_name' in parameters else None
+            if session_name:
+                return JsonResponse({"fulfillmentText": f"Information for session: {session_name}"})
+            else:
+                return JsonResponse({"fulfillmentText": "Session name not provided."})
+
+        elif intent_name == "Aster arcade URL":
+            return JsonResponse({
+                "fulfillmentText": "This is the URL of Aster Arcade: [Aster-arcade](url:https://aster.arisetech.dev/aster-arcade/)"
+            })
+
+        # Default response if no specific intent matched
+        return JsonResponse({"fulfillmentText": response.query_result.fulfillment_text})
+
+    return JsonResponse({"status": "error", "message": "This endpoint supports only POST method."})
